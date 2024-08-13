20de214c45b83b94dd58da38f4da54529a09e5b0
thagarion <th.qliphoth@gmail.com>
2024-08-13 20:50:09 +0530

    Add ping

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
 #include <dpp/dpp.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    std::string TOKEN_ID;
+    std::string TOKEN_ID = argv[1];
     uint64_t GUILD_ID;
-
-    std::cin >> TOKEN_ID >> GUILD_ID;
 
     dpp::cluster bot(TOKEN_ID);
 
@@ -16,18 +13,22 @@ int main() {
 
     bot.on_ready([&bot](const dpp::ready_t &event) {
         if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(dpp::slashcommand("ping", "Проверка", bot.me.id));
             bot.global_command_create(dpp::slashcommand("change_name", "Изменить имя на сервере", bot.me.id).add_option(
                     dpp::command_option(dpp::co_string, "name", "Новое имя")));
         }
     });
 
     bot.on_slashcommand([](const dpp::slashcommand_t &event) {
-        dpp::permission perms = event.command.get_resolved_permission(event.command.usr.id);
-        if (!perms.can(dpp::p_change_nickname)) {
-            event.reply("You don't have the required permissions to change nickname!");
-            return;
+        if (event.command.get_command_name() == "ping") {
+            event.reply("Я тут");
         }
         if (event.command.get_command_name() == "change_name") {
+            dpp::permission permission = event.command.get_resolved_permission(event.command.usr.id);
+            if (!permission.can(dpp::p_change_nickname)) {
+                event.reply("You don't have the required permissions to change nickname!");
+                return;
+            }
             std::string new_name = std::get<std::string>(event.get_parameter("name"));
             dpp::snowflake user_id = event.command.get_issuing_user().id;
             std::string nickname = event.command.get_guild().members.at(user_id).get_nickname();
@@ -36,8 +37,6 @@ int main() {
             event.reply(nickname + " теперь известен как " + guild.members.find(user_id)->second.get_mention());
         }
     });
-
-    bot.log(dpp::loglevel::ll_debug, "test");
 
     bot.start(dpp::st_wait);
 }
